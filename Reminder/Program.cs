a4898ff247d6db7bf5c5d71959e171ee00b16f6d
Roman Dudin <roman.dudin@akvelon.com>
2014-08-22 18:02:07 +0400

    middle

---
+++ Reminder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Alert!!!1111");
+        }
+    }
+}
